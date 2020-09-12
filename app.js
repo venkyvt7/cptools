5c3b2a39e7aae0191a0a8362e86be3b3cf92314d
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2020-09-12 11:41:58 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const { readFileSync, readFile } = require("fs");
+
+const home= readFileSync("main.html");
+const prime=readFileSync("prime.html");
+const  string =readFileSync("slegth.html");
+const factors=readFileSync("factors.html");
+const binary= readFileSync("binary.html");
+const port=80;
+
+const fs= require("fs");
+const http=require("http");
+console.log(123);
+
+const express= require("express");
+
+const app=express();
+
+
+app.get(  "/", (req,res)=>{
+
+
+
+res.end(home);
+
+
+
+} );
+
+app.get("/factors" , (req,res)=>{
+
+    res.end(factors);
+});
+
+app.get("/prime",(req,res)=>{ res.end(prime) });
+
+app.get("/string", (req,res)=>{res.end(string)});
+
+app.get("/binary", (req,res)=> {res.end(binary) });
+
+
+app.listen(port,  ()=>{
+
+console.log(port);
+
+
+
+})
